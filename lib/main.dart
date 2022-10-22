215378d07096e29fba8c33119f6a65ff25c88754
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2022-10-22 23:58:14 +0330

    Try Stream & async* functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,15 @@
-void main() {}
+Stream<int> boatStream() async* {
+  for (int i = 1; i <= 10; i++) {
+    print('SENT boat no.${i.toString()}');
+    await Future.delayed(const Duration(seconds: 2));
+    yield i;
+  }
+}
+
+void main() async {
+  Stream<int> stream = boatStream();
+
+  stream.listen((receivedData) {
+    print('RECEIVED boat no.${receivedData.toString()}');
+  });
+}
