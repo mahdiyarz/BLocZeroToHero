641d52e29a6031f3a5af293d9a25ca5bf04fb2a2
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2022-10-30 21:16:44 +0100

    implement hive & path provider on main

--- lib/bloc/bloc_imports.dart
+++ lib/bloc/bloc_imports.dart
@@ -2,3 +2,4 @@ export 'package:bloc/bloc.dart';
 export 'package:flutter_bloc/flutter_bloc.dart';
 export './counter_bloc.dart';
 export '../bloc/my_bloc_observer.dart';
+export 'package:hydrated_bloc/hydrated_bloc.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 import '../bloc/bloc_imports.dart';
+import 'package:path_provider/path_provider.dart';
 
 import '../app/app.dart';
 
-void main(List<String> args) {
-  Bloc.observer = MyBlocObserver();
-  runApp(const MyApp());
+void main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final storage = await HydratedStorage.build(
+      storageDirectory: await getApplicationDocumentsDirectory());
+
+  HydratedBlocOverrides.runZoned(
+    () => runApp(const MyApp()),
+    storage: storage,
+    blocObserver: MyBlocObserver(),
+  );
+
+  // Bloc.observer = MyBlocObserver();
 }
