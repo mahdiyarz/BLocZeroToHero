bfe27321d4ef457c29aa527a3d3c9efeadd9b2c7
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2022-11-11 21:27:28 +0330

    Step-1 from Vandad

---
+++ lib/vandad_nahavandipoor/home_page.dart
@@ -0,0 +1,77 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'dart:math' as math show Random;
+
+const names = [
+  'Foo',
+  'Bar',
+  'Qoo',
+];
+
+extension RandomElement<T> on Iterable<T> {
+  T getRandomElement() => elementAt(math.Random().nextInt(length));
+}
+
+class NamesCubit extends Cubit<String?> {
+  NamesCubit() : super(null);
+
+  void pickRandomName() => emit(names.getRandomElement());
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final NamesCubit cubit;
+
+  @override
+  void initState() {
+    super.initState();
+    cubit = NamesCubit();
+  }
+
+  @override
+  void dispose() {
+    cubit.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Title'),
+      ),
+      body: StreamBuilder<String?>(
+        stream: cubit.stream,
+        builder: (context, snapshot) {
+          final button = ElevatedButton(
+            onPressed: () {
+              cubit.pickRandomName();
+            },
+            child: const Text('Pick a Random Name'),
+          );
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return button;
+            case ConnectionState.waiting:
+              return button;
+            case ConnectionState.active:
+              return Column(
+                children: [
+                  Text(snapshot.data ?? ''),
+                  button,
+                ],
+              );
+            case ConnectionState.done:
+              return const SizedBox();
+          }
+        },
+      ),
+    );
+  }
+}
