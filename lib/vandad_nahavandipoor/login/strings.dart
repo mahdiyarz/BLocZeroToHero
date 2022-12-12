a60cd101420f9c2772e3d9c7f1863fc2364717e7
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2022-12-13 01:20:04 +0330

    Create strings.dart

---
+++ lib/vandad_nahavandipoor/login/strings.dart
@@ -0,0 +1,16 @@
+const emailOrPasswordEmptyDialogTitle =
+    'Please fill in both email and the password fields.';
+
+const emailOrPasswordEmptyDescription =
+    'You seem to have forgotten to enter the email or the password field, or both. Please fill in both email and the password fields.';
+
+const loginErrorDialogTitle = 'Login error';
+const loginErrorDialogContent =
+    'Invalid email/password combination. Please try again with valid login credentials!';
+
+const pleaseWait = 'Please wait...';
+const enterYourPasswordHere = 'Enter your password here...';
+const enterYourEmailHere = 'Enter your email here...';
+const ok = 'OK';
+const login = 'Log in';
+const homePage = 'Home Page';
