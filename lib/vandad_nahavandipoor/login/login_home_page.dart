bb2707e03dc1e59fb816b3893bb8abea5f0a6630
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2022-12-13 01:19:51 +0330

    Create login_home_page.dart

---
+++ lib/vandad_nahavandipoor/login/login_home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class LoginHomePage extends StatelessWidget {
+  const LoginHomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Title'),
+      ),
+      body: Container(),
+    );
+  }
+}
