87b566f954239e55edce653ecccfd68c7f155343
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2022-12-14 21:51:07 +0330

    update login home page and main

--- lib/main.dart
+++ lib/main.dart
@@ -24,11 +24,15 @@
 
 //* vandad_nahavandipoor Course
 
-import 'package:bloc_zero_to_hero/aj_flutter/bloc/bloc_imports.dart';
+import 'package:bloc_zero_to_hero/vandad_nahavandipoor/login/apis/login_api.dart';
+import 'package:bloc_zero_to_hero/vandad_nahavandipoor/login/apis/notes_api.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 // import '../vandad_nahavandipoor/cubit_home_page.dart';
-import '../vandad_nahavandipoor/bloc_home_page.dart';
-import 'vandad_nahavandipoor/bloc/persons_bloc.dart';
+import 'vandad_nahavandipoor/login/bloc/app_bloc.dart';
+import 'vandad_nahavandipoor/login/login_home_page.dart';
+// import 'vandad_nahavandipoor/persons/bloc_home_page.dart';
+// import 'vandad_nahavandipoor/persons/bloc/persons_bloc.dart';
 
 void main(List<String> args) => runApp(const MyApp());
 
@@ -39,8 +43,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: BlocProvider(
-        create: (_) => PersonsBloc(),
-        child: BlocHomePage(),
+        create: (_) => AppBloc(
+          loginApi: LoginApi(),
+          notesApi: NotesApi(),
+        ),
+        child: LoginHomePage(),
       ),
     );
   }
--- lib/vandad_nahavandipoor/login/login_home_page.dart
+++ lib/vandad_nahavandipoor/login/login_home_page.dart
@@ -1,6 +1,13 @@
+import 'package:bloc_zero_to_hero/vandad_nahavandipoor/login/bloc/actions.dart';
+import 'package:bloc_zero_to_hero/vandad_nahavandipoor/login/bloc/app_bloc.dart';
+import 'package:bloc_zero_to_hero/vandad_nahavandipoor/login/bloc/app_state.dart';
+import 'package:bloc_zero_to_hero/vandad_nahavandipoor/login/dialogs/generic_dialog.dart';
+import 'package:bloc_zero_to_hero/vandad_nahavandipoor/login/dialogs/loading_screen.dart';
+import 'package:bloc_zero_to_hero/vandad_nahavandipoor/login/models.dart';
+import 'package:bloc_zero_to_hero/vandad_nahavandipoor/login/strings.dart';
+import 'package:bloc_zero_to_hero/vandad_nahavandipoor/login/views/iterable_list_view.dart';
+import 'package:bloc_zero_to_hero/vandad_nahavandipoor/login/views/login_view.dart';
 import 'package:flutter/material.dart';
-import 'dart:convert';
-import 'dart:io';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginHomePage extends StatelessWidget {
@@ -9,9 +16,55 @@ class LoginHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Title'),
+        title: const Text(homePage),
       ),
-      body: Container(),
+      // TODO: note this widget and exp
+      body: BlocConsumer<AppBloc, AppState>(
+        listener: (context, appState) {
+          //* loading screen
+          if (appState.isLoading) {
+            LoadingScreen.instance().show(context: context, text: pleaseWait);
+          } else {
+            LoadingScreen.instance().hide();
+          }
+
+          //* display possible errors
+          final loginError = appState.loginErrors;
+          if (loginError != null) {
+            showGenericDialog(
+              context: context,
+              title: loginErrorDialogTitle,
+              content: loginErrorDialogContent,
+              optionBuilder: () => {ok: true},
+            );
+          }
+
+          //* if we are logged in, but we have no fetched notes, fetch them now
+          if (appState.isLoading == false &&
+              appState.loginErrors == null &&
+              appState.loginHandle == const LoginHandle.fooBar() &&
+              appState.fetchedNotes == null) {
+            context.read<AppBloc>().add(const LoadNoteAction());
+          }
+        },
+        builder: (context, appState) {
+          final notes = appState.fetchedNotes;
+          if (notes == null) {
+            return LoginView(
+              onLoginTapped: (email, password) {
+                context.read<AppBloc>().add(
+                      LoginAction(
+                        email: email,
+                        password: password,
+                      ),
+                    );
+              },
+            );
+          } else {
+            return notes.toListView();
+          }
+        },
+      ),
     );
   }
 }
