7f296254b8ac96b0e76c5d499344fc23fd5caa30
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2022-12-21 02:09:48 +0330

    Create bottom_bloc.dart

---
+++ lib/vandad_nahavandipoor/show_pics/bloc/bottom_bloc.dart
@@ -0,0 +1,11 @@
+import 'app_bloc.dart';
+
+class BottomBloc extends AppBloc {
+  BottomBloc({
+    Duration? waitBeforeLoading,
+    required Iterable<String> urls,
+  }) : super(
+          waitBeforeLoading: waitBeforeLoading,
+          urls: urls,
+        );
+}
