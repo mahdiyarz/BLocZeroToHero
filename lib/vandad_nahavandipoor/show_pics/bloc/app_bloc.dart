a61ca8db041ff6f9351379d892c5a53aeb2fed9e
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2022-12-24 02:19:02 +0330

    Update app_bloc.dart

--- lib/vandad_nahavandipoor/show_pics/bloc/app_bloc.dart
+++ lib/vandad_nahavandipoor/show_pics/bloc/app_bloc.dart
@@ -1,8 +1,58 @@
+import 'dart:math' as math;
+
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../bloc/app_event.dart';
 import '../bloc/app_state.dart';
 
+typedef AppBlocRandomUrlPicker = String Function(Iterable<String> allUrls);
+
+extension RandomElement<T> on Iterable<T> {
+  T getRandomElement() => elementAt(math.Random().nextInt(length));
+}
+
 class AppBloc extends Bloc<AppEvent, AppState> {
-  AppBloc() : super(const AppState.empty());
+  String _pickRandomUrl(Iterable<String> allUrls) => allUrls.getRandomElement();
+  AppBloc({
+    required Iterable<String> urls,
+    Duration? waitBeforeLoading,
+    AppBlocRandomUrlPicker? urlPicker,
+  }) : super(const AppState.empty()) {
+    on<LoadNextUrlEvent>((event, emit) async {
+      //? start loading
+      emit(
+        const AppState(
+          isLoading: true,
+          data: null,
+          error: null,
+        ),
+      );
+
+      // TODO: what is it?!!!
+      final url = (urlPicker ?? _pickRandomUrl)(urls);
+      try {
+        if (waitBeforeLoading != null) {
+          await Future.delayed(waitBeforeLoading);
+        }
+        final bundle = NetworkAssetBundle(Uri.parse(url));
+        final data = (await bundle.load(url)).buffer.asUint8List();
+        emit(
+          AppState(
+            isLoading: false,
+            data: data,
+            error: null,
+          ),
+        );
+      } catch (e) {
+        emit(
+          AppState(
+            isLoading: false,
+            data: null,
+            error: e,
+          ),
+        );
+      }
+    });
+  }
 }
